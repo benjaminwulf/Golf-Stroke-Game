c9f55a43de439c464553d9dee3417be87e92e6c5
Benjamin W Wulf <benjaminwulf@gmail.com>
2018-06-12 21:43:45 -0600

    console.log winner, loser, next hole

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,34 +1,50 @@
 $(document).ready(function () {
     console.log("connected");
     // Here we are creating a global var to display to id="number-to-guess" //bww trying to ge to id="number-to-display"
-    var numberToDisplay = 0;
+    // var numberToDisplay = 0;
     $("#number-to-guess").on("click", function () {
-        var randomNumber = 0;
+        // var randomNumber = 0;
         randomNumber = Math.floor(Math.random() * 120);
-        numberToDisplay = $("#number-to-display").html();
+        // numberToDisplay = $("#number-to-display").html();
         $("#number-to-display").text(randomNumber);
 
-        console.log("#number-to-display: ", numberToDisplay);
+        console.log("#number-to-display: ", randomNumber);
     });
 
 //=================================================================================
 
+// numberToDisplay = 0;
+var randomNumber = 0;
+var strokes = 0;
+var sumPoints = 0;
+var winCounter = 0;
+var lossCounter = 0;
+var counter = 0;
+
+//=================================================================================
+
+
 // Function that resets the game.
 function resetGame() {
     // Generate a random target number and render it to the page.
-    numberToDisplay = 0;
-    strokes = 0
+    // numberToDisplay = 0;
+    randomNumber = 0;
+    strokes = 0;
     sumPoints = 0;
+    winCounter = 0;
+    lossCounter = 0;
+    counter = 0;
   };
+
 $("#resetGame").on("click", function() {
     resetGame();
-    $("#number-to-display").text(numberToDisplay);
+    $("#number-to-display").text(randomNumber);
     $("#user-points-to-display").text(strokes);
     $("#sum-points").text(sumPoints);
 });
 
 //=================================================================================
-
+var golfBalls = randomNumGolfBall();
 
   // Function that generates random values for our crystals and returns our crystals object.
 function randomNumGolfBall() {
@@ -53,7 +69,6 @@ function randomNumGolfBall() {
     };
   };
 
-var golfBalls = randomNumGolfBall();
 
 var newDiv = "<img src='" + golfBalls.green.src + "' class='ball-image' id='ball-green'>";
     newDiv += "<img src='" + golfBalls.blue.src + "' class='ball-image' id='ball-blue'>";
@@ -64,33 +79,53 @@ $("#golf-ball").append(newDiv);
 
 //=================================================================================
 
+
+
+//=================================================================================
+
 var greenPoints = golfBalls.green.points;
 var bluePoints = golfBalls.blue.points;
 var orangePoints = golfBalls.orange.points;
 var pinkPoints = golfBalls.pink.points;
-var sumPoints = 0;
 
 $("#ball-green").on("click", function() {
     sumPoints += greenPoints;
+    gameChecker();
     $("#user-points-to-display").text(greenPoints);
     $("#sum-points").text(sumPoints);
 });
 $("#ball-blue").on("click", function() {
     sumPoints += bluePoints;
+    gameChecker();
     $("#user-points-to-display").text(bluePoints);
     $("#sum-points").text(sumPoints);
 });
 $("#ball-orange").on("click", function() {
     sumPoints += orangePoints;
+    gameChecker();
     $("#user-points-to-display").text(orangePoints);
     $("#sum-points").text(sumPoints);
 });
 $("#ball-pink").on("click", function() {
     sumPoints += pinkPoints;
+    gameChecker();
     $("#user-points-to-display").text(pinkPoints);
     $("#sum-points").text(sumPoints);
 });
 
+//=================================================================================
+
+function gameChecker() {
+    if (sumPoints == randomNumber && strokes !== 0) {
+    winCounter++;
+    console.log("Winner");
+}  else if (sumPoints > randomNumber) {
+    lossCounter++;
+    console.log("Loser");
+} else {
+    console.log("Next Hole...")
+}
+};
 
 //=================================================================================
     Array.prototype.shuffle = function(){
